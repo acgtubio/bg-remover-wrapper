71b6a8719730a7a49802273cf641b7b93c3dc071
Adrian <adrian.tubio@outlook.com>
2024-10-31 17:33:10 +0800

    add frontend

---
+++ bg-remover-frontend/src/routes/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main class="text-center mx-auto p-4 flex flex-col justify-center h-lvh items-center">
+      <h1 class="text-4xl font-bold text-gray-200 mt-20 mb-4">Background Remover</h1>
+      <h4 class="font-light italic">powered by rembg</h4>
+
+      <section class="h-2/3 border rounded-lg mt-20 p-20 max-w-7xl w-full">
+        <h1 class="text-4xl font-bold">UPLOAD FILE</h1>
+      </section>
+    </main>
+  );
+}
