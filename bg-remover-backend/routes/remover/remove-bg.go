6ff8e6f99f3c08ace2fc71a1a1bb9c8c392fb02a
Adrian <adrian.tubio@outlook.com>
2024-12-07 23:30:08 +0800

    Update route handler

--- bg-remover-backend/routes/remover/remove-bg.go
+++ bg-remover-backend/routes/remover/remove-bg.go
@@ -1,11 +1,55 @@
 package remover
 
 import (
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
+type File2Process struct {
+	originalFileName string
+	osFileName       string
+}
+
 func HandleRemoveBackground(c echo.Context) error {
-	return c.String(http.StatusOK, "Heyo successful")
+	form, err := c.MultipartForm()
+
+	if err != nil {
+		return err
+	}
+
+	files := form.File["images"]
+	fileNames := []File2Process{}
+
+	for _, file := range files {
+		src, err := file.Open()
+
+		if err != nil {
+			return err
+		}
+
+		defer src.Close()
+
+		fileName := uuid.NewString() + filepath.Ext(file.Filename)
+		dst, err := os.Create(fileName)
+		if err != nil {
+			return err
+		}
+		defer dst.Close()
+
+		fileNames = append(fileNames, File2Process{
+			originalFileName: file.Filename,
+			osFileName:       fileName,
+		})
+
+		if _, err := io.Copy(dst, src); err != nil {
+			return err
+		}
+	}
+
+	return c.String(http.StatusOK, "Oweyoweyo")
 }
